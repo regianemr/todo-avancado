e53bd60317c6fba023477736728db5b4f954f005
Regiane <regianemelo1995@gmail.com>
2024-05-27 16:07:08 -0300

    adicionando botoes e eventos

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,37 @@ const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
+const saveTodo = (text) => {
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
 
+    //criando o título
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    // criando botões
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+
+    // Para limpar o valor quando o usuário terminar de digitar
+    todoInput.value = "";
+    todoInput.focus()
+}
 
 
 // Eventos
@@ -17,7 +47,6 @@ todoForm.addEventListener("submit", (e) => {
     const inputValue = todoInput.value
 
     if(inputValue) {
-        console.log(inputValue)
-        // save todo
+        saveTodo(inputValue)
     }
 });
