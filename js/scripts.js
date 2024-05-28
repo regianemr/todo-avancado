f4d3a2b1ea86904b09b2175b9a91110f9dd9f571
Regiane <regianemelo1995@gmail.com>
2024-05-28 11:59:43 -0300

    Salvando dados no local Storage

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ const filterBtn = document.querySelector("#filter-select");
 let oldInputValue;
 
 // Funções
-const saveTodo = (text) => {
+const saveTodo = (text, done = 0, save = 1) => {
     const todo = document.createElement("div")
     todo.classList.add("todo")
 
@@ -38,6 +38,15 @@ const saveTodo = (text) => {
     deleteBtn.classList.add("remove-todo")
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
     todo.appendChild(deleteBtn)
+
+    // Utilizando dados da localStorage
+    if(done) {
+        todo.classList.add("done")
+    }
+
+    if(save) {
+        saveTodoLocalStorage({text, done})
+    }
 
     todoList.appendChild(todo)
 
@@ -184,3 +193,20 @@ filterBtn.addEventListener("change", (e) => {
 
     filterTodos(filterValue)
 })
+
+
+// Local Storage
+const getTodosLocalStorage = () => {
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+    return todos;
+}
+
+const saveTodoLocalStorage = (todo) => {
+    const todos = getTodosLocalStorage()
+
+    todos.push(todo)
+
+    localStorage.setItem("todos", JSON.stringify(todos))
+
+}
