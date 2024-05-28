a043767f8ed8a8253bb974224d4c49e71d7d4f8a
Regiane <regianemelo1995@gmail.com>
2024-05-28 11:10:08 -0300

    editando tarefas

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue
+
 // Funções
 const saveTodo = (text) => {
     const todo = document.createElement("div")
@@ -39,6 +41,26 @@ const saveTodo = (text) => {
     todoInput.focus()
 }
 
+// fazendo aparecer apenas a parte da edição
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+
+}
+
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text
+        }
+    })
+}
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
@@ -50,3 +72,46 @@ todoForm.addEventListener("submit", (e) => {
         saveTodo(inputValue)
     }
 });
+
+document.addEventListener("click", (e) => {
+
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div") //selecionou o elemento pai div mais próximo
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText
+    }
+
+    if(targetEl.classList.contains("finish-todo"))
+        parentEl.classList.toggle("done")
+
+    if(targetEl.classList.contains("remove-todo")) {
+        parentEl.remove()
+    }
+
+    if(targetEl.classList.contains("edit-todo")) {
+      toggleForms()  
+
+      editInput.value = todoTitle
+      oldInputValue = todoTitle
+    }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
+})
